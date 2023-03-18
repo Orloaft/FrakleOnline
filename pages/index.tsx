f606f644f7e2347aa1a0c8b020efc9c169004cb3
unknown <alexorlow17@gmail.com>
2023-03-18 03:09:29 -0400

    fix title for mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,13 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.home}>
-      <h1 className="title">Space Dice</h1>
+      <h1 className="title">
+        <span>Space</span>
+        <span>Dice</span>{" "}
+      </h1>
 
-      <Link href="/game">
-        <div className={styles.play}>
-          <div className={styles.letter}>p</div>
-          <div className={styles.letter}>l</div>
-          <div className={styles.letter}>a</div>
-          <div className={styles.letter}>y</div>
-        </div>
+      <Link href="/game" className="title" style={{ textDecoration: "none" }}>
+        PLAY
       </Link>
     </div>
   );
